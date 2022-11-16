1188314899b55a458f64e2bfa87629ffc6284f41
lavverma <lavverma109@gmail.com>
2022-11-17 00:08:30 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ChakraProvider} from "@chakra-ui/react"
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +12,4 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
-reportWebVitals();
+// reportWebVitals();
